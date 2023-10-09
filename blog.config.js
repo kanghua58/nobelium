d0b9ff42c6cd1d1765f0b950e480f928fc6ee1bf
Kanghua <88917038+kanghua58@users.noreply.github.com>
2023-10-09 13:36:21 -0400

    changed font style

--- blog.config.js
+++ blog.config.js
@@ -7,7 +7,7 @@ const BLOG = {
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'US/Eastern', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
-  font: 'Inter', // ['sans-serif', 'serif']
+  font: 'Roboto', // ['sans-serif', 'serif']
   lightBackground: '#fffff', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#18181B', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
